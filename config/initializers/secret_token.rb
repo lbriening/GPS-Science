7873111e3ef01ec984fbef708241145bb4c7d03e
Andrew Briening <abriening@gmail.com>
2013-12-28 19:08:49 -0500

    Initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GpsScience::Application.config.secret_key_base = '9389e667df1ead2420d2180c75b27758166e089bd9a8e21885445a5c547866df33a34fd5240d7fd4aad513288b290b12d0098a6e0be9a59c5632831867acc159'
